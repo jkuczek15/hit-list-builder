37ea393e9ded7cfa61d089d616c0c17cd8b1f918
Joe Kuczek <joe.kuczek@gmail.com>
2018-03-31 02:18:35 -0500

    added base website files'

---
+++ assets/header.php
@@ -0,0 +1,31 @@
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>Hit-List Builder</title>
+  <link rel="icon" href="assets/img/favicon.png">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/jumbotron.css">
+</head>
+
+<nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">Hit-List Builder</a>
+
+  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+##################################
+##################################
+# HEADER                         #
+##################################
+##################################
+include('assets/header.php');
+
+##################################
+##################################
+# BODY                           #
+##################################
+##################################
+?>
+<div class="jumbotron">
+  <div class="container">
+    <h1 class="display-3">Hit-List Builder</h1>
+    <p>Search for products, build a results list, find contacts quickly and efficiently. This message should probably be replaced with something better.</p>
+    <p style="margin-top: 3em;"><a href="#" role="button"><img src="assets/img/google-login.png" width="30%"/></a></p>
+  </div>
+</div>
+<?
+##################################
+##################################
+# FOOTER                         #
+##################################
+##################################
+include('assets/footer.php');
